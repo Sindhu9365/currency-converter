6fc9abd2c515a77c025a3c6d6ddd1e5f80274681
Sindhu9365 <24msrdf034@jainuniversity.ac.in>
2025-05-13 10:56:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -14,14 +15,18 @@ rates = {
 def index():
     result = None
     if request.method == 'POST':
-        amount = float(request.form['amount'])
-        from_currency = request.form['from_currency']
-        to_currency = request.form['to_currency']
+        try:
+            amount = float(request.form['amount'])
+            from_currency = request.form['from_currency']
+            to_currency = request.form['to_currency']
 
-        converted_amount = (amount / rates[from_currency]) * rates[to_currency]
-        result = f"{amount} {from_currency} = {round(converted_amount, 2)} {to_currency}"
+            converted_amount = (amount / rates[from_currency]) * rates[to_currency]
+            result = f"{amount} {from_currency} = {round(converted_amount, 2)} {to_currency}"
+        except Exception as e:
+            result = f"Error: {e}"
     
     return render_template('index.html', result=result, rates=rates.keys())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Render sets this automatically
+    app.run(host='0.0.0.0', port=port)
